936546a6ddac482ff353fdee257e55c0c41f19e7
Jack Hayhurst <jhayhurst@liquidweb.com>
2017-07-12 23:00:55 -0400

    got the syntax working on the script

--- zshrc.d/ssh_color.zsh
+++ zshrc.d/ssh_color.zsh
@@ -1,75 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
-host_color() {
-	emulate -LR zsh
-
-  local -x host=""
-
-  if [[ $# -lt 2 ]]; then
-    print "you have to specify a host to connect to"
-    exit 1
-  fi
-
-  # pop off the ssh command on the start
-  local parts=${$@[2,$#]}
-  local pos=1
-
-  while [[ ${pos} -lt $#parts  ]] ; do
-
-    case "$parts[${pos}]" in
-      ^-*[b|c|D|E|e|F|I|i|L|Q|R|S|W|w|p])
-      # these are multi-argument flags - they can have a space after them or no
-      if [[ $parts[${pos}] ~= '^-.*?(b|c|D|E|e|F|I|i|L|Q|R|S|W|w|p)\ [^\ -]' ]]; then
-        # if there is a space between this arg and the next, skip twice
-        ((pos++))
-      fi
-      ;;
-      ^-*)
-      ;;
-      *)
-      # add that position in the array to the host slice
-      host+=($parts[${pos}])
-      ;;
-    esac
-    ((pos++))
-  done
-
-  unset pos
-  unset parts
-
-  host=$host[1]
-  if [[ $host[1] ~= ^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) ]]; then
-    if [[ $(dig +short -x $match[1]) != "" ]]; then
-      # if there's a ptr record, set that to the hostname
-      host=$(dig +short -x $match[1])
-    else
-      host=$match[1]
-    fi
-  elif host ~= ^([0-9a-fA-F:]*); then
-    if [[ $(dig +short -x $match[1]) != "" ]]; then
-      # if there's a ptr record, set that to the hostname
-      host=$(dig +short -x $match[1])
-    else
-      host=$match[1]
-    fi
-  fi
-
-  hash=$(echo ${host}|openssl md5|awk '{print $2}')
-
-  set -x ret_val
-
-  color=$(host_color_string_replace(${hash:0:1}))
-  color+=${hash:1:2}
-  color+=$(host_color_string_replace(${hash:2:3}))
-  color+=${hash:3:4}
-  color+=$(host_color_string_replace(${hash:5:6}))
-  color+=${hash:6:7}
-
-  echo ${color}
-}
+set -uo pipefail
 
 host_color_string_replace() {
-  if [ -z $1 ]; then
+  if [ -z ${1} ]; then
     return 2
   fi
 
@@ -100,4 +34,75 @@ host_color_string_replace() {
     ;;
   esac
 
+}
+
+host_color() {
+
+	#emulate -LR zsh
+
+  if [[ $# -lt 2 ]]; then
+    print "you have to specify a host to connect to"
+    exit 1
+  fi
+
+  # pop off the ssh command on the start
+  local parts=${$@[2,$#]}
+  local pos=1
+  while [[ ${pos} -lt "${#parts[@]}" ]] ; do
+    case "$parts[${pos}]" in
+      ^-*[b|c|D|E|e|F|I|i|L|Q|R|S|W|w|p])
+      # these are multi-argument flags - they can have a space after them or no
+      local multi_flags='^\-.*?(b|c|D|E|e|F|I|i|L|Q|R|S|W|w|p)\ +-'
+      if [[ $parts[${pos}] =~ ${} ]]; then
+        # if there is a space between this arg and the next, skip twice
+        ((pos++))
+      fi
+      unset multi_flags
+      ;;
+      ^-*)
+      ;;
+      *)
+      # add that position in the array to the host slice
+      host+=($parts[${pos}])
+      ;;
+    esac
+    ((pos++))
+  done
+
+  unset pos
+  unset parts
+  local -x host=""
+
+  host=$host[1]
+  local ipv4='^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'
+  local ipv6='^([0-9a-fA-F:]*)'
+
+  if [[ ${host} =~ ${ipv4} ]]; then
+    if [[ $(dig +short -x $match[1]) != "" ]]; then
+      # if there's a ptr record, set that to the hostname
+      host=$(dig +short -x $match[1])
+    else
+      host=$match[1]
+    fi
+  elif [[ ${host} =~ ${ipv6} ]] ; then
+    if [[ $(dig +short -x $match[1]) != "" ]]; then
+      # if there's a ptr record, set that to the hostname
+      host=$(dig +short -x $match[1])
+    else
+      host=$match[1]
+    fi
+  fi
+
+  local hash=$(echo ${host}|openssl md5|awk '{print $2}')
+
+  local -x ret_val
+
+  local color=$(host_color_string_replace ${hash:0:1})
+  color+=${hash:1:2}
+  color+=$(host_color_string_replace ${hash:2:3})
+  color+=${hash:3:4}
+  color+=$(host_color_string_replace ${hash:5:6})
+  color+=${hash:6:7}
+
+  echo ${color}
 }
