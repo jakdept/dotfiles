4db8f738c22df18668505e090ae4fc6768b8b88c
Jack Hayhurst <jakdept@gmail.com>
2017-07-24 21:57:09 -0400

    multiple changes to clean up go
    - removed PROJECTS alias
    - removed GOPATH - it's now automatic

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -1,4 +1,3 @@
-export GOPATH=$PROJECTS/go # set my go code directory
+#export PATH=$PATH:/usr/local/go/bin # add the go system binary path
 export PATH="$GOPATH/bin:$PATH" # add my binary to the front of my path
-export PATH=$PATH:/usr/local/go/bin # add the go system binary path
 export CDPATH=$CDPATH:.:$GOPATH/src
