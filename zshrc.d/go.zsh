3621a3f2cc8c4a57a80294f63ccc12e399cdefd8
Jack Hayhurst <jakdept@gmail.com>
2017-07-25 21:52:44 -0400

    fixed gopath and cdpath

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -1,3 +1,4 @@
 #export PATH=$PATH:/usr/local/go/bin # add the go system binary path
+export GOPATH=${HOME}/go
 export PATH="$GOPATH/bin:$PATH" # add my binary to the front of my path
-export CDPATH=$CDPATH:.:$GOPATH/src
+export CDPATH=$CDPATH:$GOPATH/src
