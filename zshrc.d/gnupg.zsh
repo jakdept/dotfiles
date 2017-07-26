172f1b19501d97ec5513b2ff66417a28024321a2
Jack Hayhurst <jakdept@gmail.com>
2017-07-26 00:37:44 -0400

    added the starts of gpg stuff for yubikey

---
+++ zshrc.d/gnupg.zsh
@@ -0,0 +1,34 @@
+# eval $(gpg-agent --daemon)
+# GPG_TTY=$(tty)
+# export GPG_TTY
+# if [ -f "${HOME}/.gpg-agent-info" ]; then
+#     . "${HOME}/.gpg-agent-info"
+#     export GPG_AGENT_INFO
+#     export SSH_AUTH_SOCK
+# fi
+# 
+# gpg! () {
+#   echo "kill gpg-agent"
+#   code=0
+#   while [ 1 -ne $code ]; do
+#     killall gpg-agent
+#     code=$?
+#     sleep 1
+#   done
+# 
+#   echo "kill ssh"
+#   killall ssh
+# 
+#   echo "kill ssh muxers"
+#   for pid in `ps -ef | grep ssh | grep -v grep | awk '{print $2}'`; do
+#     kill $pid
+#   done
+# 
+#   echo "restart gpg-agent"
+#   eval $(gpg-agent --daemon)
+# 
+#   echo
+#   echo "All done. Now unplug / replug the NEO token."
+#   echo
+# }
+# 
