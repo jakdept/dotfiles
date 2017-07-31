86f46c6d6500bf446a5d5b459ab9d7d5a8f3ca00
Jack Hayhurst <jakdept@gmail.com>
2017-07-31 02:59:02 -0400

    moved ip/host function to itself

--- zshrc.d/ssh_color.zsh
+++ zshrc.d/ssh_color.zsh
@@ -25,50 +25,13 @@ ssh_color_string_replace() {
 
 }
 
-ssh-color() {
-  if [[ $# -lt 1 ]]; then
-    print "you have to specify a host to connect to"
-    exit 1
-  fi
+ssh-change-title() {
+  # sets the title
+  printf "\033]0;$*\007"
+}
 
-  # pop off the ssh command on the start
-  local part=$(echo $@|sed \
-  -e 's/-[^ bcDEeFIiLQRSWwp]*[bcDEeFIiLQRSWwp]\ [^ ]*\ //g' \
-  -e 's/-[^ ]*\ //g' \
-  -e 's/.*@//g' \
-  -e 's/\ .*$//g')
-
-  local host=""
-  local ip=""
-
-  local ipv4='^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})$'
-  local ipv6='^([0-9a-fA-F:]*)$'
-
-  if [[ ${part} =~ ${ipv4} ]]; then
-    ip=${part}
-    host=$(dig +short -x ${part}|head -n1)
-  elif [[ ${part} =~ ${ipv6} ]] ; then
-    ip=${part}
-    host=$(dig +short -x ${part}|head -n1)
-  else
-    ip=$(dig +short ${part}|head -n1)
-    host=${part}
-  fi
-
-  local hash=$(echo ${host} ${ip}|openssl md5|awk '{print $NF}')
-
-  local hexHash=$(echo ${hash}|awk '{print substr($0, 5, 6)}')
-
-  local hexColor=""
-  hexColor+=$(ssh_color_string_replace ${hexHash[1]})
-  hexColor+=${hexHash[2]}
-  hexColor+=$(ssh_color_string_replace ${hexHash[3]})
-  hexColor+=${hexHash[4]}
-  hexColor+=$(ssh_color_string_replace ${hexHash[5]})
-  hexColor+=${hexHash[6]}
-
-  # you can set the title for everything - idk
-  printf "\033]0;${host} - ${ip}\007"
+ssh-change-color() {
+  local hexColor=${1}
 
   local rColor=$(echo ${hexColor}|awk '{printf "%d", "0x" substr($0, 0, 2)}')
   local gColor=$(echo ${hexColor}|awk '{printf "%d", "0x" substr($0, 2, 2)}')
@@ -93,28 +56,79 @@ ssh-color() {
       printf "\033]11;#${hexColor}\007"
     fi
   fi
+}
 
-  # echo debugging ${host} ${ip} ${hash} ${hexHash} ${hexColor} ${rColor} ${gColor} ${bColor}
+ssh-color-get-dest () {
+
+  # pop off the ssh command on the start
+  local part=$(echo $@|sed \
+  -e 's/-[^ bcDEeFIiLQRSWwp]*[bcDEeFIiLQRSWwp]\ [^ ]*\ //g' \
+  -e 's/-[^ ]*\ //g' \
+  -e 's/.*@//g' \
+  -e 's/\ .*$//g')
+
+  echo ${part}
+}
+
+
+ssh-color-get-host-ip() {
+  local dest=$1
+
+  local host=""
+  local ip=""
+
+  local ipv4='^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})$'
+  local ipv6='^([0-9a-fA-F:]*)$'
+
+  if [[ ${dest} =~ ${ipv4} ]]; then
+    ip=${dest}
+    host=$(dig +short -x ${dest}|head -n1)
+  elif [[ ${dest} =~ ${ipv6} ]] ; then
+    ip=${dest}
+    host=$(dig +short -x ${dest}|head -n1)
+  else
+    ip=$(dig +short ${dest}|head -n1)
+    host=${dest}
+  fi
+
+  echo ${ip} ${host}
+}
+
+ssh-color() {
+  if [[ $# -lt 1 ]]; then
+    print "you have to specify a host to connect to"
+    exit 1
+  fi
+
+  local dest=$(ssh-color-get-dest $*)
+
+  local ip=
+  local host=
+
+  read ip host <<(ssh-color-get-host-ip ${dest})
+
+  local hash=$(echo ${host} ${ip}|openssl md5|awk '{print $NF}')
+
+  local hexHash=$(echo ${hash}|awk '{print substr($0, 15, 6)}')
+
+  local hexColor=""
+  hexColor+=$(ssh_color_string_replace ${hexHash[1]})
+  hexColor+=${hexHash[2]}
+  hexColor+=$(ssh_color_string_replace ${hexHash[3]})
+  hexColor+=${hexHash[4]}
+  hexColor+=$(ssh_color_string_replace ${hexHash[5]})
+  hexColor+=${hexHash[6]}
+
+  # echo debugging ${hash} ${hexHash} ${hexColor} ${host} ${ip} ${rColor} ${gColor} ${bColor}
+
+  ssh-change-title " ${host} - [${ip}] "
+  ssh-change-color ${hexColor} 
 
   ssh $*
-
   local retCode=$?
 
-  if [[ -z ${SSH_COLOR+x} ]]; then
-    if [[ "$TERM" = "screen"* ]] && [[ -n "$TMUX" ]] ; then
-      tmux select-pane -P "bg=#000000"
-    elif [[ ${TERM_PROGRAM} == "Apple_Terminal" ]] ; then
-      osascript -e "tell application \"Terminal\"
-        set background color of current settings of selected tab of front window to { 0, 0, 0 }
-      end tell"
-    elif [[ ${TERM_PROGRAM} == "iTerm.app" ]] ; then
-      osascript -e "tell application \"iTerm\"
-        set background color of current session of front window to { 0, 0, 0 }
-      end tell"
-    else
-      printf "\033]11;#000000\007"
-    fi
-  fi
+  ssh-change-title " localhost "
+  ssh-change-color 000000
 
   return ${retCode}
 }
