2a77137705b9ee021300b3b15a9102c846bf3898
Jack Hayhurst <jakdept@gmail.com>
2017-08-14 17:36:41 -0400

    moved osc stuff into docker_func and added openssl

--- zshrc.d/docker_func.zsh
+++ zshrc.d/docker_func.zsh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-#osc_enter() {
-#  docker exec -it osc /bin/bash
-#}
+openssl () {
+	docker run -it --rm svagi/openssl "$@"
+}
 
-
-
-osc_init() {
+osc() {
   # kill the image if it is running
   osc_kill
 
@@ -22,13 +20,7 @@ docker run -it --name=osc \
           -v "$(pwd):/root" \
           -w "/root" \
           jaltek/docker-opensuse-osc-client /bin/bash
-#  docker create --name=osc \
-#          -v "$(pwd):/root" \
-#          jaltek/docker-opensuse-osc-client /bin/bash
-#  docker start osc
 }
-
-
 
 osc_kill() {
   [[ $(docker ps -a|grep osc|wc -l) -gt 0 ]] && {
